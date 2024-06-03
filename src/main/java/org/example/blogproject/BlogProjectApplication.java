3367002f7ba6c7931cb7a5a8d218f6c03a14702a
BMDevOps <kumupaf6998@naver.com>
2024-06-03 19:52:18 +0900

    기본 셋팅

--- src/main/java/org/example/blogproject/BlogProjectApplication.java
+++ src/main/java/org/example/blogproject/BlogProjectApplication.java
@@ -2,7 +2,9 @@ package org.example.blogproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
 @ComponentScan({"org.example.blogproject.*"})
@@ -11,5 +13,5 @@ public class BlogProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogProjectApplication.class, args);
     }
-
 }
+
